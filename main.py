0e4ff1208528b12974c8e386e508f667bb90c436
marciof2 <marcinsilva26@gmail.com>
2026-02-23 19:34:12 -0300

    initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+import pygame
+
+pygame.init()
+
+window = pygame.display.set_mode(size=(800, 600))
+
+while True:
+    #Check for all events
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            event.quit() #close window
+            quit() #end pygame
+
+
+
+
+
